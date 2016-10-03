c4dd50456b1751ba0c6b615fdb1b782b5059ccd2
Dyhoereos <dennis.chau@outlook.com>
2016-10-02 21:13:14 -0700

    Pickers: Completed double picker. Fixed single picker issue

--- Pickers/Pickers/DoubleComponentPickerViewController.swift
+++ Pickers/Pickers/DoubleComponentPickerViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var doublePicker: UIPickerView!
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = ["ham", "Turkey", "Peanut Butter", " Tuna Salad", "Chicken Salad", "Roast Beef", "Vegemite"]
+    private let breadTypes = ["white", "Whole Wheat", "Rye", "Sourdough", "Seven Grain"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +28,33 @@ class DoubleComponentPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+        
+        let message = "Your \(filling) on \(bread) bread will be right up."
+        let alert = UIAlertController(title: "Thank you for your order", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Great", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-    */
+    
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == breadComponent ? breadTypes.count : fillingTypes.count
+    }
+
+    // MARK:-
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return component == breadComponent ? breadTypes[row] : fillingTypes[row]
+    }
 
 }
