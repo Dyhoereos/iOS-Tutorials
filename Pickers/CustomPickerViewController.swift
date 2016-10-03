b4fa3e4aeba212c898f9317dd93eec06851d2a22
Dyhoereos <dennis.chau@outlook.com>
2016-10-03 11:10:56 -0700

    Pickers: customer picker created. Unknown bug causing crash when switching to customer picker

--- Pickers/CustomPickerViewController.swift
+++ Pickers/CustomPickerViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class CustomPickerViewController: UIViewController {
-
+class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    private var images:[UIImage]!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        images = [
+            UIImage(named: "seven")!,
+            UIImage(named: "bar")!,
+            UIImage(named: "crown")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "lemon")!,
+            UIImage(named: "apple")!
+        ]
+        winLabel.text = " "
+        arc4random_stir()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +34,46 @@ class CustomPickerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func spin(_ sender: AnyObject) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        for i in 0..<5 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow += 1
+            } else {
+                numInRow = 1
+            }
+            lastVal = newValue
+            
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            win = numInRow >= 3 ? true : false
+        }
+        winLabel.text = win ? "WINNER!" : " "
     }
-    */
-
+    
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    // MARK:-
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+    }
 }
